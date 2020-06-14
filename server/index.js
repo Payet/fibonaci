f0e9a6b56dd08d39fc40553dd1a7168626154b9d
Bertrand Payet <bertrand@Bertrands-MBP.fritz.box>
2020-06-14 21:18:11 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -81,11 +81,6 @@ app.post('/values', async (req, res) => {
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-  //added
-  redisClient.get("values", function(err, reply) {
-      // reply is null when the key is missing
-      console.log("Redis values: " + reply);
-  });
 
   res.send({ working: true });
 });
